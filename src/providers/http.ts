0d5675d5c9979bb90bec8182e04b1c75d4c8673e
undertow <undertow@abc.com>
2022-10-04 22:35:16 +0900

    0.1 安装daisy ui之前

--- src/providers/http.ts
+++ src/providers/http.ts
@@ -1,65 +1,76 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse, AxiosRequestConfig } from 'axios'
 
 export const httpProvider = axios.create({
   baseURL: process.env.SERVER_API_URL,
   timeout: 60000,
-});
+})
 //isBrowser = true
-const isBrowser = typeof window !== 'undefined';
+const isBrowser = typeof window !== 'undefined'
 //interceptors axios拦截器
 httpProvider.interceptors.request.use(
-  (config) => {
+  // 在发送请求之前做些什么
+  (config: AxiosRequestConfig) => {
     if (isBrowser) {
-      const token = window.localStorage.getItem('token');
+      const token = window.localStorage.getItem('token')
       if (config && config.headers && token) {
-        config.headers.Authorization = `Bearer ${token}`;
+        config.headers.Authorization = `Bearer ${token}`
       }
     }
-    return config;
+    return config
   },
+  // 对请求错误做些什么
   () => {
-    throw new Error('发起请求出错');
+    throw new Error('发起请求出错')
   }
-);
+)
 
 httpProvider.interceptors.response.use(
   (
-    data: AxiosResponse<{
-      statusCode: number;
-      success: boolean;
-      msg: string | null;
-      data: unknown;
+    value: AxiosResponse<{
+      statusCode: number
+      success: boolean
+      msg: string | null
+      data: unknown //value.data.data 。。。
     }>
   ) => {
-    const res = data.data;
-
+    // res=value.data
+    const res = value.data
     if (!res.success) {
-      console.error(res.msg);
-      return null;
+        console.log(res)
+      console.log(res.msg + ' failed')
+      return res.msg
     }
-    return res.data;
+    return res.msg + ' data successfully'
   },
-  (err) => {
+  err => {
     if (err && err.response && err.response.status) {
-      const status = err.response.status;
+      const status = err.response.status
 
       switch (status) {
         case 400:
         case 404:
         case 504:
-          isBrowser && console.error((err.response && err.response.data && err.response.data.msg) || '服务器异常');
-          break;
+          isBrowser &&
+            console.error(
+              (err.response && err.response.data && err.response.data.msg) ||
+                '服务器异常'
+            )
+          break
 
         default:
-          isBrowser && console.error((err.response && err.response.data && err.response.data.msg) || '未知错误!');
-          break;
+          isBrowser &&
+            console.error(
+              (err.response && err.response.data && err.response.data.msg) ||
+                '未知错误!'
+            )
+          break
       }
       return Promise.reject({
         statusCode: err.response.status,
         message: err.response.data.msg,
-      });
+      })
     }
 
-    return Promise.reject(err);
+    return Promise.reject(err)
   }
-);
+)
